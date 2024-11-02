b66b0f4725ea464a90d496c62a93307385787425
oswpadron <oswaldopadron99@gmail.com>
2024-11-02 17:08:10 -0400

    Debugging

--- db/php1.sql
+++ db/php1.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE IF NOT EXISTS php1-oswpadron
+CREATE DATABASE IF NOT EXISTS php1_oswpadron
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_general_ci;
 
-USE php1;
+USE php1_oswpadron;
 
 CREATE TABLE IF NOT EXISTS usuarios (
   cedula INT(10) PRIMARY KEY NOT NULL,
