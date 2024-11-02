e4e5e559cf8d7e2a006fb113d2ab4663ef73ebe1
oswpadron <oswaldopadron99@gmail.com>
2024-11-02 17:47:50 -0400

    new updates

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+$link = mysqli_connect("localhost", "root", "", "php1_oswpadron");
+
+if (!$link) {
+  die('Error de Conexión (' . mysqli_connect_errno() . ') '
+    . mysqli_connect_error());
+}
+
+echo 'Éxito... ' . mysqli_get_host_info($link) . "\n";
+?>
