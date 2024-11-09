379a6d3dba045934cb7e149b044349e231f4e819
oswpadron <oswaldopadron99@gmail.com>
2024-11-09 11:17:22 -0400

    Finished

--- conexion.php
+++ conexion.php
@@ -1,10 +1,7 @@
 <?php
-$link = mysqli_connect("localhost", "root", "", "php1_oswpadron");
-
+$link = mysqli_connect("localhost", "root", "", "php1");
 if (!$link) {
-  die('Error de Conexión (' . mysqli_connect_errno() . ') '
-    . mysqli_connect_error());
+die('Error de Conexión (' . mysqli_connect_errno() . ') '
+. mysqli_connect_error());
 }
-
-echo 'Éxito... ' . mysqli_get_host_info($link) . "\n";
 ?>
